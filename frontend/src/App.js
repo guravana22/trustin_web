84aee79ea227a94798a8177357b617378a47aac0
guravana22 <guravana22@gmail.com>
2024-07-24 12:35:05 +0530

    #1 removed the unwanted code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,10 @@
 import React from "react";
-// import Login from "./components/Login";
 import Routers from "./components/Routers";
-// import Header from "./components/Header";
-// import Footer from "./components/Footer";
 
 function App() {
   return (
     <React.Fragment>
       <Routers />
-      {/* <Header />
-      <Footer /> */}
     </React.Fragment>
   );
 }
