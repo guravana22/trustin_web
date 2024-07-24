af26e1dd613b525aa497e8672aeef68fad6eb830
guravana22 <guravana22@gmail.com>
2024-07-24 13:56:55 +0530

    #1 changed the social media icons

--- frontend/src/components/Clients.jsx
+++ frontend/src/components/Clients.jsx
@@ -3,6 +3,27 @@ import admlogo from "../assets/images/admlogo.svg";
 import fsrlogo from "../assets/images/fsrlogo.svg";
 import emiterslogo from "../assets/images/emiterslogo.svg";
 import centralbanklogo from "../assets/images/centeralbanklogo.svg";
+import OwlCarousel from "react-owl-carousel";
+
+const options = {
+  // stagePadding: 60,
+  nav: false,
+  margin: 40,
+  loop: true,
+  dots: false,
+  touchDrag: true,
+  responsive: {
+    0: {
+      items: 3,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 4,
+    },
+  },
+};
 
 const clientLogos = [
   { src: admlogo, alt: "ADM Logo" },
@@ -15,14 +36,22 @@ const Clients = () => {
   return (
     <React.Fragment>
       <div className="client_section text-center">
-        <div className="container">
+        <div className="container-fluid">
           <h3>We are affiliated with</h3>
-          <div className="row mt-5 ms-4 me-4 flex-wrap">
-            {clientLogos.map((logo, index) => (
-              <div className="col" key={index}>
-                <img src={logo.src} alt={logo.alt} className="mb-3" />
-              </div>
-            ))}
+          <div className="row mt-5 justify-content-center justify-content-md-none">
+            <div class="col col-lg-9 col-sm-12">
+              <OwlCarousel className="owl-theme screenshot_slider" {...options}>
+                {clientLogos.map((logo, index) => (
+                  <div className="col" key={index}>
+                    <img
+                      src={logo.src}
+                      alt={logo.alt}
+                      className="mb-3 m-auto client_img"
+                    />
+                  </div>
+                ))}
+              </OwlCarousel>
+            </div>
           </div>
         </div>
       </div>
